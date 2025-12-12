c73569f25bb77a2d565f0bef5a364b0d938655da
Ben Wiklund <ben@daisyowl.com>
2025-12-11 22:35:38 -0800

    :pensive:

--- src/day11.rs
+++ src/day11.rs
@@ -26,8 +26,6 @@ pub fn solve(part: u32) -> u64 {
 
     match part {
         0 => {
-            dbg!(&devices);
-            // the problem doesn't say no loops, but if there were loops then the answer would be infinity. so there must not be.
             // let seen = HashSet::<String>::new();
             fn search(devices: &HashMap<String, Device>, id: &str) -> u64 {
                 match id {
@@ -42,7 +40,38 @@ pub fn solve(part: u32) -> u64 {
             search(&devices, "you")
         }
 
-        1 => 0,
+        1 => {
+            // different enough that i don't wanna share the traversal code.
+            // the addition of dac/fft is a wrinkle but the main problem here is that srv->out goes through way more nodes than you->out.
+            // i think this is a dp problem, where we do math on the number of ways we've reached a node as we get there. so we need a bfs that has a list of (node, tally) we're cascading out to (possibly multiple copies of each node, merged), and keeps a running tally of how many paths got us there
+            // ok nevermind this is how the fft/dac requirement fucks us lol
+            // 
+            // fn search(
+            //     devices: &HashMap<String, Device>,
+            //     id: &str,
+            //     seen_dac: bool, // no need to get too fancy
+            //     seen_fft: bool, // no need to get too fancy
+            // ) -> u64 {
+            //     match id {
+            //         "out" => (seen_dac && seen_fft) as u64,
+            //         _ => devices[id]
+            //             .outputs
+            //             .iter()
+            //             .map(|oid| {
+            //                 search(
+            //                     devices,
+            //                     oid,
+            //                     seen_dac || id == "dac",
+            //                     seen_fft || id == "fft",
+            //                 )
+            //             })
+            //             .sum::<u64>(),
+            //     }
+            // }
+            // search(&devices, "svr", false, false)
+
+            0
+        }
 
         _ => panic!(),
     }
@@ -54,7 +83,7 @@ mod tests {
 
     #[test]
     fn day11() {
-        assert_eq!(solve(0), 0);
-        // assert_eq!(solve(1), 0);
+        assert_eq!(solve(0), 523);
+        assert_eq!(solve(1), 0);
     }
 }
