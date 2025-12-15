afafcf8331f8c6784fac8df700799440c26a4b18
Ben Wiklund <ben@daisyowl.com>
2025-12-14 22:53:40 -0800

    d2p2

--- y2024/src/day2.rs
+++ y2024/src/day2.rs
@@ -10,19 +10,32 @@ pub fn solve(part: u32) -> u64 {
         .collect::<Vec<Vec<i64>>>();
 
     match part {
-        0 => reports
+        0 => reports.iter().filter(|r| is_safe(&r)).count() as u64,
+
+        1 => reports
             .iter()
             .filter(|r| {
-                let diffs: Vec<i64> = r.windows(2).map(|w| w[1] - w[0]).collect();
-                diffs.iter().all(|d| (1..=3).contains(&d.abs()))
-                    && (diffs.iter().all(|&d| d > 0) || diffs.iter().all(|&d| d < 0))
+                is_safe(r)
+                    || (0..r.len()).any(|remove_idx| {
+                        is_safe(
+                            &r.iter()
+                                .enumerate()
+                                .filter(|&(idx, _)| idx != remove_idx)
+                                .map(|(_, &val)| val)
+                                .collect(),
+                        )
+                    })
             })
             .count() as u64,
 
-        1 => 0,
-
         _ => unreachable!(),
     }
+}
+
+fn is_safe(r: &Vec<i64>) -> bool {
+    let diffs: Vec<i64> = r.windows(2).map(|w| w[1] - w[0]).collect();
+    diffs.iter().all(|d| (1..=3).contains(&d.abs()))
+        && (diffs.iter().all(|&d| d > 0) || diffs.iter().all(|&d| d < 0))
 }
 
 #[cfg(test)]
@@ -32,6 +45,6 @@ mod tests {
     #[test]
     fn day9() {
         assert_eq!(solve(0), 334);
-        // assert_eq!(solve(1), 0);
+        assert_eq!(solve(1), 400);
     }
 }
