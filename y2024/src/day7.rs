ce04fc3c5e43d0a75136d6845ace0265da580f76
Ben Wiklund <ben@daisyowl.com>
2025-12-17 05:55:41 -0800

    d7p2

--- y2024/src/day7.rs
+++ y2024/src/day7.rs
@@ -1,22 +1,30 @@
 #[derive(Debug)]
 struct Equation {
-    result: i64,
-    operands: Vec<i64>,
+    result: u64,
+    operands: Vec<u64>,
 }
 
-fn can_be_solved(eq: &Equation, i: usize, acc: i64) -> bool {
+fn can_be_solved(eq: &Equation, allow_pipe_op: bool, i: usize, acc: u64) -> bool {
     if acc > eq.result {
         return false; // since we are only add+mult of positive numbers, we can never decrease
     }
 
     if let Some(next) = eq.operands.get(i) {
-        can_be_solved(eq, i + 1, acc * next) || can_be_solved(eq, i + 1, acc + next)
+        can_be_solved(eq, allow_pipe_op, i + 1, acc * next)
+            || can_be_solved(eq, allow_pipe_op, i + 1, acc + next)
+            || (allow_pipe_op && can_be_solved(eq, allow_pipe_op, i + 1, pipe_op(acc, *next)))
     } else {
         eq.result == acc
     }
 }
 
-pub fn solve(part: u32) -> i64 {
+fn pipe_op(a: u64, b: u64) -> u64 {
+    // 123 || 456 = 123456
+    let a_mult = 10u64.pow(((b as f64).log10() + 1f64).floor() as u32);
+    a * a_mult + b
+}
+
+pub fn solve(part: u32) -> u64 {
     let eqs = std::fs::read_to_string("./src/day7_input.txt")
         .unwrap()
         .lines()
@@ -33,11 +41,15 @@ pub fn solve(part: u32) -> i64 {
     match part {
         0 => eqs
             .iter()
-            .filter(|eq| can_be_solved(eq, 1, eq.operands[0]))
+            .filter(|eq| can_be_solved(eq, false, 1, eq.operands[0]))
             .map(|eq| eq.result)
             .sum(),
 
-        1 => 0,
+        1 => eqs
+            .iter()
+            .filter(|eq| can_be_solved(eq, true, 1, eq.operands[0]))
+            .map(|eq| eq.result)
+            .sum(),
 
         _ => unreachable!(),
     }
@@ -50,6 +62,6 @@ mod tests {
     #[test]
     fn day7() {
         assert_eq!(solve(0), 2941973819040);
-        // assert_eq!(solve(1), 0);
+        assert_eq!(solve(1), 249943041417600);
     }
 }
