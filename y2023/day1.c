2493339fe15ee7ca912f81b58e2fe8eabc2c00d6
Ben Wiklund <ben@daisyowl.com>
2025-12-17 17:08:31 -0800

    d1p1

--- y2023/day1.c
+++ y2023/day1.c
@@ -1,11 +1,40 @@
 #include <stdio.h>
+#include <stdbool.h>
 
-int main() {
-  char ch;
-  FILE *f = fopen("day1_input.txt", "r");
+int main()
+{
+    char ch;
+    FILE *f = fopen("day1_input.txt", "r");
 
-  while ((ch = fgetc(f)) != EOF) printf("%c", ch);
+    int first_num = -1; // how to avoid magic numbers like this in c. or is this just how we roll
+    int last_num = -1;
+    int sum = 0;
 
-  fclose(f);
-  return 0;
+    while ((ch = fgetc(f)) != EOF)
+    {
+        bool is_number = ch >= 48 && ch <= 57;
+        if (is_number)
+        {
+            int number = ch - 48;
+            if (first_num == -1)
+            {
+                first_num = number;
+            }
+            last_num = number;
+        }
+        if (ch == '\n')
+        {
+            // printf("%d, %d\n", first_num, last_num);
+            int calibration_number = first_num * 10 + last_num;
+            sum += calibration_number;
+
+            first_num = -1;
+            last_num = -1;
+        }
+    }
+    fclose(f);
+
+    printf("%d\n", sum);
+
+    return 0;
 }
