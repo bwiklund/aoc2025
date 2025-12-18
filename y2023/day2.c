ecec9b8fff56be6a62a80028101796a8754ff2ac
Ben Wiklund <ben@daisyowl.com>
2025-12-17 23:33:12 -0800

    asdfasdf

--- y2023/day2.c
+++ y2023/day2.c
@@ -3,76 +3,75 @@
 #include <stdlib.h>
 #include <string.h>
 
-int accept_char(FILE *f, char ch) {
-    if (fgetc(f) == ch) {
-        return true;
-    } else {
-        ungetc(1, f);
-        return false;
-    }
-}
+typedef struct {
+    int count;
+} Reveal;
 
-int accept_str(FILE *f, const char *str) {
-    int len = strlen(str);
-    for (int i = 0; i < len; i++) {
-        if (!accept_char(f, str[i])) {
-            ungetc(i, f);
-            return false;
-        }
-    }
-    return true;
-}
-
-int accept_digit(FILE *f) {
-    char ch = fgetc(f);
-    if ((ch >= '0' && ch <= '9')) {
-        return ch - '0';
-    } else {
-        ungetc(1, f);
-        return -1; // fail
-    }
-}
-
-int accept_number(FILE *f) {
-    int acc = 0;
-    int d;
-    int count = 0;
-    while ((d = accept_digit(f)) != -1) {
-        count += 1;
-        acc *= 10;
-        acc += d;
-    }
-    if (count == 0) {
-        return -1; // fail
-    }
-    return acc;
+int peekc(FILE *f) {
+    int ch = fgetc(f);
+    if (ch != EOF)
+        ungetc(ch, f);
+    return ch;
 }
 
 int parse_game(FILE *f) {
-    if (accept_str(f, "Game ") == -1) {
-        return -1; // fail
-    }
-    int game_id;
-    if ((game_id = accept_number(f)) == -1) {
-        return -1; // fail
-    }
-    printf("offset: %c\n", (char)fgetc(f));
-    printf("offset: %c\n", (char)fgetc(f));
-    if (accept_str(f, ": ") == -1) {
+    int id;
+    char line[256];
+    if (!fgets(line, sizeof line, f)) {
         return -1;
     }
-    printf("offset: %d\n", game_id);
-    int n;
-    if ((n = accept_number(f)) == -1) {
-        return -1;
-    }
-    printf("game id: %d %d\n", game_id, n);
-    if (accept_str(f, ": ") == -1) {
-        return -1;
-    }
-    // while ((fgetc(f) != '\n'))
-    //     ;
-    return n;
+
+    sscanf(line, "Game %d: ", &id);
+    printf("\nGame: %d\n", id);
+
+    sscanf(line, "%d", &id);
+    printf("\nGame: %d\n", id);
+
+    return 0;
+
+    // Reveal reveals[100];
+    // while (true) {
+    //     int count;
+    //     char word[64];
+    //     sscanf(f, "%d %63[a-z]", &count, word);
+    //     printf("%d %s\n", count, word);
+
+    //     int after = fgetc(f);
+    //     if (after == '\n') {
+    //         break;
+    //     } else if (after == ';') {
+    //         sscanf(f, " ");
+    //         continue;
+    //     } else if (after == ',') {
+    //         sscanf(f, " ");
+    //         continue;
+    //     }
+    // }
+    // if (accept_str(f, "Game ") == -1) {
+    //     return -1; // fail
+    // }
+    // int game_id;
+    // if ((game_id = accept_number(f)) == -1) {
+    //     return -1; // fail
+    // }
+    // printf("offset: %c\n", (char)fgetc(f));
+    // printf("offset: %c\n", (char)fgetc(f));
+    // if (accept_str(f, ": ") == -1) {
+    //     return -1;
+    // }
+    // printf("offset: %d\n", game_id);
+    // int n;
+    // if ((n = accept_number(f)) == -1) {
+    //     return -1;
+    // }
+    // printf("game id: %d %d\n", game_id, n);
+    // if (accept_str(f, ": ") == -1) {
+    //     return -1;
+    // }
+    // // while ((fgetc(f) != '\n'))
+    // //     ;
+    // return n;
+    // return 0;
 }
 
 int parse_input(FILE *f) {
