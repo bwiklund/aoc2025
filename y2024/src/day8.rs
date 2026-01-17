fd8d0da19b003695525ed79c15bbf6afe99c9b8d
Ben Wiklund <ben@daisyowl.com>
2026-01-18 12:27:13 +1300

    d8p2

--- y2024/src/day8.rs
+++ y2024/src/day8.rs
@@ -29,7 +29,7 @@ pub fn solve(part: u32) -> i64 {
 
     match part {
         0 => {
-            let mut unique_antimodes = HashSet::<(i32, i32)>::new();
+            let mut unique_antinodes = HashSet::<(i32, i32)>::new();
             for a in nodes.iter() {
                 for b in nodes.iter() {
                     if a == b {
@@ -40,15 +40,43 @@ pub fn solve(part: u32) -> i64 {
                         let anti_x = a.0 + (b.0 - a.0) * 2;
                         let anti_y = a.1 + (b.1 - a.1) * 2;
                         if anti_x >= 0 && anti_x < w && anti_y >= 0 && anti_y < h {
-                            unique_antimodes.insert((anti_x, anti_y));
+                            unique_antinodes.insert((anti_x, anti_y));
                         }
                     }
                 }
             }
-            unique_antimodes.len() as i64
+            unique_antinodes.len() as i64
         }
 
-        1 => 0,
+        1 => {
+            let mut unique_antinodes = HashSet::<(i32, i32)>::new();
+            for a in nodes.iter() {
+                for b in nodes.iter() {
+                    if a == b {
+                        continue;
+                    }
+                    if a.2 == b.2 {
+                        // calculate antinodes beyond b until we run off the map
+                        let mut x = a.0;
+                        let mut y = a.1;
+                        let (x2, y2, _) = b;
+                        let dx = x2 - x;
+                        let dy = y2 - y;
+
+                        loop {
+                            x += dx;
+                            y += dy;
+                            if x >= 0 && x < w && y >= 0 && y < h {
+                                unique_antinodes.insert((x, y));
+                            } else {
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+            unique_antinodes.len() as i64
+        }
 
         _ => unreachable!(),
     }
@@ -60,7 +88,7 @@ mod tests {
 
     #[test]
     fn day8() {
-        assert_eq!(solve(0), 0);
-        // assert_eq!(solve(1), 0);
+        assert_eq!(solve(0), 354);
+        assert_eq!(solve(1), 0);
     }
 }
