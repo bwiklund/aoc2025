cc43cdc37c375aef236818017dbaf1d4b9785936
Ben Wiklund <ben@daisyowl.com>
2026-01-24 11:46:03 +1300

    refactor

--- y2024/src/day9.rs
+++ y2024/src/day9.rs
@@ -1,5 +1,12 @@
+struct FileSpan {
+    id: Option<i32>,
+    len: i32,
+}
+
+type Disk = Vec<Option<i32>>;
+
 pub fn solve(part: u32) -> i64 {
-    let mut disk = parse_input();
+    let mut disk = parse_input_sparse().to_disk();
 
     match part {
         0 => {
@@ -11,10 +18,7 @@ pub fn solve(part: u32) -> i64 {
                     while first_free_idx < disk.len() && disk[first_free_idx].is_some() {
                         first_free_idx += 1;
                     }
-                    if first_free_idx >= disk.len() {
-                        break;
-                    }
-                    if first_free_idx >= idx {
+                    if first_free_idx >= disk.len() || first_free_idx >= idx {
                         break;
                     }
                     disk[first_free_idx] = disk[idx];
@@ -30,7 +34,19 @@ pub fn solve(part: u32) -> i64 {
     }
 }
 
-fn checksum(disk: Vec<Option<i32>>) -> i64 {
+trait ToDisk {
+    fn to_disk(&self) -> Disk;
+}
+
+impl ToDisk for Vec<FileSpan> {
+    fn to_disk(&self) -> Disk {
+        self.iter()
+            .flat_map(|span| (0..span.len).map(|_| span.id))
+            .collect()
+    }
+}
+
+fn checksum(disk: Disk) -> i64 {
     disk.iter()
         .enumerate()
         .map(|(i, n)| match n {
@@ -40,29 +56,20 @@ fn checksum(disk: Vec<Option<i32>>) -> i64 {
         .sum()
 }
 
-fn parse_input() -> Vec<Option<i32>> {
-    let mut disk = vec![];
-    let mut id = 0;
-    let mut phase_is_file = true;
-    for ch in std::fs::read_to_string("./src/day9_input.txt")
+fn parse_input_sparse() -> Vec<FileSpan> {
+    std::fs::read_to_string("./src/day9_input.txt")
         .unwrap()
         .chars()
-    {
-        if let Some(count) = ch.to_digit(10) {
-            for _ in 0..count {
-                if phase_is_file {
-                    disk.push(Some(id));
-                } else {
-                    disk.push(None);
-                }
-            }
-            if phase_is_file {
-                id += 1;
-            }
-            phase_is_file = !phase_is_file;
-        }
-    }
-    disk
+        .filter_map(|ch| ch.to_digit(10))
+        .enumerate()
+        .map(|(idx, len)| FileSpan {
+            id: match idx % 2 == 0 {
+                true => Some((idx / 2) as i32),
+                false => None,
+            },
+            len: len as i32,
+        })
+        .collect()
 }
 
 #[cfg(test)]
@@ -72,6 +79,6 @@ mod tests {
     #[test]
     fn day9() {
         assert_eq!(solve(0), 6337367222422);
-        // assert_eq!(solve(1), 0);
+        assert_eq!(solve(1), 0);
     }
 }
